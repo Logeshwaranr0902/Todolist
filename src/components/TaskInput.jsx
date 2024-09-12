4e157f78b0a8baa816ea1994fb25766e2931a43f
Your Name <you@example.com>
2024-09-12 18:38:26 +0530

    first commit

---
+++ src/components/TaskInput.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+const TaskInput = ({addTask}) => {
+    const [task, setTask] = useState('');
+
+    function handleInputValue(e) {
+        setTask(e.target.value)
+    }
+    function handleAddTask(e){
+        e.preventDefault();
+        if (task.trim() === '') return;
+        addTask(task)
+        setTask('')
+    }
+  return (   
+    <form className='inputField ' onSubmit={handleAddTask}>
+        <input type = "text" value={task} placeholder='Add Item...' onChange={handleInputValue} />
+        <button>+</button>
+    </form>
+  )
+}
+
+export default TaskInput
